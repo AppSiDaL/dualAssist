ef34c67fbeb61af89c3cc4f481c39f1e62717b1e
AppSiDaL <gilielcrack343@gmail.com>
2024-06-05 11:33:01 -0600

    chore: Remove unnecessary code and update API endpoint in main.py

--- main.py
+++ main.py
@@ -2,8 +2,17 @@
 
 from fastapi import FastAPI
 
+
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    lastname: str
+    time: str
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.post("/")
+async def root(item: Item):
+    return {
+        "numeroControl": item.lastname,
+        "time": item.time
+    }
