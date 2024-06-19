1fe6e6d256e8e320b03b8f95be8309c4cebc657f
AppSiDaL <gilielcrack343@gmail.com>
2024-06-19 10:13:29 -0600

    chore: Refactor main.py to improve database functionality and API endpoint

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 # main.py
+# Importamos las librerias necesarias
+# Importamos os para manejar las variables de entorno
 import os
+
+# Importamos load_dotenv para cargar las variables de entorno
 from dotenv import load_dotenv
+
+# Importamos FastAPI para crear la API
 from fastapi import FastAPI
+
+# Importamos BaseModel para tipear la entrada de la API
 from pydantic import BaseModel
-from datetime import datetime
+
+# Importamos datetime para obtener la fecha y hora actual
+from datetime import datetime, timedelta
+
+# Importamos psycopg2 para conectarnos a la base de datos
 import psycopg2
 
-load_dotenv()  # take environment variables from .env.
+load_dotenv()  # Cargamos las variables de entorno
 
 
+# Creamos la clase Item que hereda de BaseModel para la entrada de la API
 class Item(BaseModel):
     lastname: str
 
 
+# Función para conectarnos a la base de datos
 def conectar_bd():
-    # Establecemos la conexión con la base de datos PostgreSQL
     cnx = psycopg2.connect(
         host=os.environ.get("DB_HOST"),
         port=os.environ.get("DB_PORT"),
@@ -25,7 +38,9 @@ def conectar_bd():
     return cnx
 
 
+# Creamos la aplicación FastAPI
 app = FastAPI()
+# Obtenemos las variables de entorno
 HOST = os.getenv("HOST")
 USER = os.getenv("USER")
 PASSWORD = os.getenv("PASSWORD")
@@ -33,24 +48,137 @@ PORT = os.getenv("PORT")
 DATABASE = os.getenv("DATABASE")
 
 
+# Definimos la ruta raíz de la API
 @app.post("/")
 async def root(item: Item):
-    current_time = datetime.now().isoformat()
+    # Obtenemos la fecha y hora actual
+    hoy = datetime.now().date()
+    hora_actual = datetime.now().time()
+    # Obtenemos el número de control del alumno y recortamos los espacios
     numeroControl = item.lastname.strip()
-    print(numeroControl)
-    insertar_datos(numeroControl, current_time)
-    return {"numeroControl": numeroControl, "time": current_time}
+    # Consultamos si el alumno ya registro asistencia antes
+    resultado = consultar_entrada(numeroControl)
+    # Si el alumno ya registro asistencia antes
+    if resultado:
+        # Retornamos un mensaje de que el alumno ya registro asistencia antes
+        return {
+            "numeroControl": numeroControl,
+            "fecha": hoy,
+            "hora": hora_actual,
+            "mensaje": "El alumno ya registro asistencia antes.",
+        }
+    # Si el alumno no ha registrado asistencia antes
+    else:
+        # Consultamos si el alumno ya registro asistencia hoy
+        resultado = consultar_entrada_hoy(numeroControl)
+        # Si el alumno ya registro asistencia hoy
+        if resultado:
+            # Insertamos el registro de salida del alumno
+            insertar_dato_salida(numeroControl, hoy, hora_actual)
+            # Retornamos un mensaje de que el alumno ha registrado su salida
+            return {
+                "numeroControl": numeroControl,
+                "fecha": hoy,
+                "hora": hora_actual,
+                "mensaje": "El alumno ha registrdo su salida.",
+            }
+        else:
+            # Insertamos el registro de asistencia del alumno
+            insertar_dato_entrada(numeroControl, hoy, hora_actual)
+            # Retornamos un mensaje de que el alumno ha registrado su asistencia
+            return {
+                "numeroControl": numeroControl,
+                "fecha": hoy,
+                "hora": hora_actual,
+                "mensaje": "El alumno ha registrado su asistencia.",
+            }
 
 
-def insertar_datos(numeroControl, time):
+def consultar_entrada(numeroControl):
     conexion = conectar_bd()
     cursor = conexion.cursor()
-    insert_query = "INSERT INTO asistencia (numero_control,entrada) VALUES (%s, %s)"
+    hoy = datetime.now().date()
+    hora_actual = datetime.now().time()
+    # Calcular la hora 30 minutos antes
+    hora_inicio = (datetime.now() - timedelta(minutes=30)).time()
+    # Consultar si el alumno ya registro asistencia
+    query = """
+    SELECT * FROM asistencia
+    WHERE numero_control = %s AND fecha = %s
+    AND hora_entrada BETWEEN %s AND %s
+    """
+    cursor.execute(
+        query,
+        (
+            numeroControl,
+            str(hoy),
+            str(hora_inicio),
+            str(hora_actual),
+        ),
+    )
+    resultado = cursor.fetchall()
+
+    cursor.close()
+    conexion.close()
+    return resultado
+
+
+def consultar_entrada_hoy(numeroControl):
+    conexion = conectar_bd()
+    cursor = conexion.cursor()
+    hoy = datetime.now().date()
+    # Consultar si el alumno ya registro asistencia hoy
+    query = """
+    SELECT * FROM asistencia
+    WHERE numero_control = %s AND fecha = %s
+    """
+    cursor.execute(
+        query,
+        (
+            numeroControl,
+            str(hoy),
+        ),
+    )
+    resultado = cursor.fetchall()
+
+    cursor.close()
+    conexion.close()
+    return resultado
+
+
+def insertar_dato_entrada(numeroControl, fecha, hora_actual):
+    conexion = conectar_bd()
+    cursor = conexion.cursor()
+    # Insertar el registro de asistencia del alumno
+    insert_query = (
+        "INSERT INTO asistencia (numero_control,fecha,hora_entrada) VALUES (%s,%s, %s)"
+    )
     cursor.execute(
         insert_query,
         (
             numeroControl,
-            time,
+            str(fecha),
+            str(hora_actual),
+        ),
+    )
+    conexion.commit()
+    cursor.close()
+    conexion.close()
+
+
+def insertar_dato_salida(numeroControl, fecha, hora_actual):
+    conexion = conectar_bd()
+    cursor = conexion.cursor()
+    # Insertar el registro de salida del alumno
+    insert_query = (
+        "INSERT INTO asistencia (numero_control,fecha,hora_salida) VALUES (%s,%s, %s)"
+    )
+    cursor.execute(
+        insert_query,
+        (
+            numeroControl,
+            str(fecha),
+            str(hora_actual),
         ),
     )
     conexion.commit()
